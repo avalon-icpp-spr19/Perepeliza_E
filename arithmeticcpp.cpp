fe8f7fa8739005fc7ff8d4e928b42117fc47dc0f
egorpere <47290148+egorpere@users.noreply.github.com>
2019-02-16 20:26:10 +0300

    Add files via upload

---
+++ arithmeticcpp.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	/*ÇÀÄÀÍÈÅ 1*/
+	/*int a;
+	cin >> a;
+	int first = a / 100;
+	int second = a / 10 % 10;
+	int third = a % 10;
+	cout << first + second + third << endl;*/
+
+
+
+	/*ÇÀÄÀÍÈÅ 2*/
+	/*int usertime;
+	int first_h;
+	int first_m;
+	int first_s;
+	cin >> first_h >> first_m  >> first_s;
+	int second_h;
+	int second_m;
+	int second_s;
+	cin >> second_h >> second_m >> second_s;
+	int total_sec_first = first_h * 60 * 60 + first_m * 60 + first_s;
+	int total_sec_second = second_h * 60 * 60 + second_m * 60 + second_s;
+	int difference = total_sec_second - total_sec_first;
+	cout << difference;*/
+
+
+
+
+	/*ÇÀÄÀÍÈÅ 3*/
+	//int day;
+	//cin >> day;
+
+	//int month;
+	//cin >> month;
+
+	//int year;
+	//cin >> year;
+
+	///*êîıôôèöåíòû*/
+	//int a;
+	//a = (14 - month) / 12;
+	//int y;
+	//y = year + 4800 - a;
+	//int m;
+	//m = month + 12 * a - 3;
+
+	//int JDN;
+	//JDN = day + ((153 * m + 2) / 5) + (365 * y) + (y / 4) - (y / 100) + (y/400) - 32045;
+
+	//cout << JDN;
+
+
+
+
+	/*ÇÀÄÀÍÈÅ 4*/
+	/*int users_sec;
+	cin >> users_sec;
+	int hours = (users_sec / 60) / 60;
+	cout <<  hours << ':';
+	users_sec = users_sec - (hours * 60 * 60 );
+	int minutes;
+	minutes = users_sec / 60;
+	int ten_m;
+	ten_m = minutes / 10;
+	minutes = minutes - ten_m * 10;
+	cout << ten_m << minutes << ':';
+	users_sec = users_sec - (minutes * 60 + ten_m * 600);
+	int seconds;
+	seconds = users_sec;
+	int ten_s;
+	ten_s = seconds / 10;
+	seconds = seconds - ten_s * 10;
+	cout << ten_s << seconds;*/
+
+
+	/*ÇÀÄÀÍÈÅ 5*/
+	//int a;
+	//cin >> a;
+	//int b;
+	//b = ((a % 2) - 2) * (-1) + a;
+	//cout << b;
+
+	/*ÇÀÄÀÍÈÅ 6*/
+	//int a;
+	//cin >> a;
+	//int first, second, third, thourth;
+	//first = a / 1000;
+	//second = (a / 100) % 10;
+	//third = (a % 100) / 10;
+	//thourth = a % 10;
+	//int result;
+	//result = abs(thourth - first) + abs(third - second) + 1;
+	//cout << result;
+
+	/*ÄÎÏÎËÍÈÒÅËÜÍÎÅ ÇÀÄÀÍÈÅ*/
+	//float h, a, b;
+	//cin >> h >> a >> b;
+	//float result;
+	//result = ceil((h - b) / (a - b));
+	//cout << result;
+}
