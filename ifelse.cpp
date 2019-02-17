6dd9f74dfdb14a7ae50d4946cb2c3c4fbbafbd68
egorpere <47290148+egorpere@users.noreply.github.com>
2019-02-17 13:29:00 +0300

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	///*ÇÀÄÀÍÈÅ 1*/
+
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	if ((a + b) > c && (a + c) > b && (b + c) > a)
+	{
+		cout << "òàêîé òðåóãîëüíèê ñóùåñòâóåò" << endl;
+	}
+	else
+	{
+		cout << "òàêîé òðåóãîëüíèê íå ñóùåñòâóåò" << endl;
+	}*/
+
+
+
+	///*ÇÀÄÀÍÈÅ 2*/
+	//int coin;
+	//cin >> coin;
+	//int last_count = coin % 10;
+	//		switch (last_count)
+	//		{
+	//		case 1:
+	//			cout << coin << " ðóáëü" << endl;
+	//			break;
+	//		case 2:
+	//		case 3:
+	//		case 4:
+	//			cout << coin << " ðóáëÿ" << endl;
+	//			break;
+	//		default:cout << coin << " ðóáëåé" << endl;
+	//			break;
+	//		}
+
+
+
+	///*ÇÀÄÀÍÈÅ 3*/
+	//bool enemyInFront;
+	//bool isBoss;
+	//int robotHealth;
+	//cin >> enemyInFront >> isBoss >> robotHealth;
+	//bool shouldFire = true;
+	//if (enemyInFront == true && isBoss == true)
+	//{
+	//	if (robotHealth < 50) shouldFire = false;
+	//	if (robotHealth > 100) shouldFire = true;
+	//}
+	//else
+	//{
+	//	shouldFire = false;
+	//}
+
+
+
+	//if (shouldFire)
+	//{
+	//	cout << "Fire" << endl;
+
+	//}
+	//else
+	//{
+	//	cout << "No fire" << endl;
+	//}
+
+
+
+	///*ÇÀÄÀÍÈÅ 5*/
+	//int day, mounth, year;
+	//cin >> day >> mounth >> year;
+
+	//bool vesokos = false;
+	//if (year % 400 == 0 || ((year % 100 != 0) && (year % 4 == 0)))
+	//{
+	//	vesokos = true;
+	//}
+
+
+	//	switch (mounth)
+	//	{
+	//	case 1:
+	//	case 3:
+	//	case 5:
+	//	case 7:
+	//	case 8:
+	//	case 10:
+	//	case 12:
+	//	{
+	//		if (day <= 31) { cout << "True"; }
+	//		else { cout << "Flase"; }
+	//		break;
+	//	}
+	//	case 4:
+	//	case 6:
+	//	case 9:
+	//	case 11:
+	//	{
+	//		if (day <= 30) { cout << "True"; }
+	//		else { cout << "Flase"; }
+	//		break;
+	//	}
+	//	default:
+	//		if (vesokos) 
+	//		{
+	//			if (day <= 29) { cout << "True"; }
+	//			else { cout << "Flase"; }
+	//		}
+	//		else { cout << "Flase"; }
+	//		break;
+	//	}
+
+
+
+///*ÇÀÄÀÍÈÅ 4*/
+	//int a, b, c;
+	//cin >> a >> b >> c;
+
+	//int max = (a > b) ? a : b;
+	//max = (max > c) ? max : c;
+
+
+	//int min = (a < b) ? a : b;
+	//min = (min < c) ? min : c;
+
+	//int medium;
+	//if (a != min && a != max) { medium = a; }
+	//else if (b != min && b != max) { medium = b; }
+	//else if (c != min && c != max) { medium = c; }
+
+	//cout << medium;
+}
